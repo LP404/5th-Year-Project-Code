16cf295e1dfc6fa49757f9df975350466a8fc841
LP404 <71828222+LP404@users.noreply.github.com>
2022-10-31 18:06:23 +0000

    Changed Variable Names

--- MultiDirPlotter.py
+++ MultiDirPlotter.py
@@ -75,34 +75,34 @@ Val1, Val2 = 3, 0.035
 suffix = ".txt"
 
 
-
-# for i in range(len(files)):
-#     files[i] = files[i][:-len(suffix)]  
+for i in range(len(dirs)):
+    for j in range(len(vars()['files'+dirs[i]])):
+        vars()['files'+dirs[i]][j] = vars()['files'+dirs[i]][j][:-len(suffix)]  
+        
+        vars()[vars()['files'+dirs[i]][j]] = np.loadtxt(open(path + "\\" + vars()['files'+dirs[i]][j] + ".txt", "rb"), delimiter=",",skiprows = 2).T
+        vars()[vars()['files'+dirs[i]][j]+'newY'] = NoiseFilter(Val1,Val2,vars()[vars()['files'+dirs[i]][j]][1])
+        
+        
+        
+        
+        plt.figure(i)
+        plt.plot(vars()[vars()['files'+dirs[i]][j]][0],vars()[vars()['files'+dirs[i]][j]][1],label = files[j])
+        plt.plot(vars()[vars()['files'+dirs[i]][j]][0],vars()[vars()['files'+dirs[i]][j]+'newY'],label = files[j]+' filtered')
+        Point = np.where(np.gradient(vars()[vars()['files'+dirs[i]][j]+'newY']) == max(np.gradient(vars()[vars()['files'+dirs[i]][j]+'newY'])))[0][0]
+        m,c,err = LineValFinder(vars()[vars()['files'+dirs[i]][j]][0],vars()[vars()['files'+dirs[i]][j]+'newY'],10)
+        yFit = m * vars()[vars()['files'+dirs[i]][j]][0] + c
+        Contstraint = (yFit >= 0) & (yFit <= max(vars()[vars()['files'+dirs[i]][j]+'newY']))
+        
+        line1start = (min(vars()[vars()['files'+dirs[i]][j]][0]),0)
+        line1end = (max(vars()[vars()['files'+dirs[i]][j]][0]),0)    
+        
+        line2start = (min(vars()[vars()['files'+dirs[i]][j]][0][Contstraint]),min(yFit[Contstraint]))
+        line2end = (max(vars()[vars()['files'+dirs[i]][j]][0][Contstraint]),max(yFit[Contstraint]))
     
-#     vars()[files[i]] = np.loadtxt(open(path + "\\" + files[i] + ".txt", "rb"), delimiter=",",skiprows = 2).T
-#     vars()[files[i]+'newY'] = NoiseFilter(Val1,Val2,vars()[files[i]][1])
-    
-    
-    
-    
-#     plt.figure(i)
-#     plt.plot(vars()[files[i]][0],vars()[files[i]][1],label = files[i])
-#     plt.plot(vars()[files[i]][0],vars()[files[i]+'newY'],label = files[i]+' filtered')
-#     Point = np.where(np.gradient(vars()[files[i]+'newY']) == max(np.gradient(vars()[files[i]+'newY'])))[0][0]
-#     m,c,err = LineValFinder(vars()[files[i]][0],vars()[files[i]+'newY'],10)
-#     yFit = m * vars()[files[i]][0] + c
-#     Contstraint = (yFit >= 0) & (yFit <= max(vars()[files[i]+'newY']))
-    
-#     line1start = (min(vars()[files[i]][0]),0)
-#     line1end = (max(vars()[files[i]][0]),0)    
-    
-#     line2start = (min(vars()[files[i]][0][Contstraint]),min(yFit[Contstraint]))
-#     line2end = (max(vars()[files[i]][0][Contstraint]),max(yFit[Contstraint]))
-
-#     vars()[files[i]+'Intercept'] = line_intersection((line1start,line1end),(line2start,line2end))    
-    
-#     # plt.plot(vars()[files[i]][0][Contstraint],yFit[Contstraint],label = 'Intercept = '+str(np.around(vars()[files[i]+'Intercept'][0],2)))
-#     plt.xlabel('Wavelength (nm)')
-#     plt.ylabel('Transmittance, E_bg =' + str(np.around(vars()[files[i]+'Intercept'][0],2)))
-#     plt.title(files[i])
-#     plt.legend()
+        vars()[files[j]+'Intercept'] = line_intersection((line1start,line1end),(line2start,line2end))    
+        
+        # plt.plot(vars()[vars()['files'+dirs[i]][j]][0][Contstraint],yFit[Contstraint],label = 'Intercept = '+str(np.around(vars()[files[j]+'Intercept'][0],2)))
+        plt.xlabel('Wavelength (nm)')
+        plt.ylabel('Transmittance, E_bg =' + str(np.around(vars()[files[j]+'Intercept'][0],2)))
+        plt.title(vars()['files'+dirs[i]][j])
+        plt.legend()
