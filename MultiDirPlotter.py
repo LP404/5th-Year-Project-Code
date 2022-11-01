e2b85abd6f6af210058ac532da1188f91725cd5f
LP404 <71828222+LP404@users.noreply.github.com>
2022-11-01 19:20:09 +0000

    Plots same sample on different dates, on the same graph

--- MultiDirPlotter.py
+++ MultiDirPlotter.py
@@ -18,7 +18,6 @@ Val1, Val2 = 3, 0.035
 
 suffix = ".txt"
 
-
 for i in range(len(dirs)):
     for j in range(len(vars()['files'+dirs[i]])):
         vars()['files'+dirs[i]][j] = vars()['files'+dirs[i]][j][:-len(suffix)]  
@@ -47,6 +46,51 @@ for i in range(len(dirs)):
         
         # plt.plot(vars()[vars()['files'+dirs[i]][j]][0][Contstraint],yFit[Contstraint],label = 'Intercept = '+str(np.around(vars()[vars()['files'+dirs[i]][j]+'Intercept'][0],2)))
         plt.xlabel('Wavelength (nm)')
-        plt.ylabel('Transmittance, E_bg =' + str(np.around(vars()[vars()['files'+dirs[i]][j]+'Intercept'][0],2)))
+        bgEn = (3e8 * 6.63e-34)/(vars()[vars()['files'+dirs[i]][j]+'Intercept'][0]*1e-9)
+        bgeV =  np.around((6.242e18 * bgEn),2)
+        plt.ylabel('Transmittance, E_bg =' + str(bgeV)+'eV')
         plt.title(vars()['files'+dirs[i]][j] + " " + dirs[i])
         plt.legend()
+        
+
+prefix = ( "01","02","03","04","05","06","07","08","09","0A","0B","0C","0D","0E","0F")
+
+SamBin = [[] for _ in range(len(prefix))]
+
+
+
+prefixDict = { "01" : "Ag3BiI6_I"
+          ,"02" : "Ag3BiI6_II"
+          ,"03" : "Cu2AgBiI6_V"
+          ,"04" : "Cu2AgBiI6_VI"
+          ,"05" : "CuAgBiI5_VII"
+          ,"06" : "CuAgBiI5_VIII"
+          ,"07" : "Ag2BiI5_IX"
+          ,"08" : "Ag2BiI5_X"
+          ,"09" : "Ag2BiI5_B1"
+          ,"0A" : "Ag2BiI5_B2"
+          ,"0B" : "Ag3BiI6_B2"
+          ,"0C" : "null"
+          ,"0D" : "null"
+          ,"0E" : "null"
+          ,"0F" : "null"}
+        
+for i in range(len(dirs)):
+    for j in range(len(vars()['files'+dirs[i]])):
+        SamBin[prefix.index(vars()['files'+dirs[i]][j][0:2])].append([vars()[vars()['files'+dirs[i]][j]][0],vars()[vars()['files'+dirs[i]][j]+'newY'],vars()['files'+dirs[i]][j][0:2],dirs[i]])
+        
+SamBin = list(filter(None,SamBin))
+
+for i in range(len(SamBin)):
+    for j in range(len(SamBin[i])):
+        plt.figure(i+1*200)
+        plt.title(prefixDict[SamBin[i][j][2]])
+        plt.ylabel('Transmittance (%)')
+        plt.xlabel('wavelength (nm)')
+        plt.plot(SamBin[i][j][0],SamBin[i][j][1], label = SamBin[i][j][3])
+        plt.legend()
+        
+        
+
+
+
